572aac6c53c8e95cfdeb438ab5b67f13e86abd9d
DiogoCC7 <dassuncao1234@gmail.com>
2024-10-05 21:28:12 +0100

    refactor: Update Migrations

--- migrations/001-table.sql
+++ migrations/001-table.sql
@@ -1,18 +1,18 @@
 -- Create Authenticate Table
 create table auths(
-    id uuid primary key,
-    email varchar(50) default not null,
-    password text default not null,
-    salt text default not null,
+    id uuid not null,
+    email varchar(50) not null,
+    password text not null,
+    salt text not null,
     is_active boolean default false,
     role int default 0, -- 0 USER / 1 ORGANIZATION / 2 SPONSOR / 3 ADMIN
     primary key (id)
-)
+);
 
 -- Create Profile Table
 create table profiles(
-    id uuid primary key,
-    auth_id uuid default not null,
+    id uuid not null,
+    auth_id uuid not null,
     type int default 0, -- 0 MAIN / 1 SUB
     primary key (id),
     CONSTRAINT auth_id_fk
