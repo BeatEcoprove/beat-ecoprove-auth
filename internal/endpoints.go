28b971fd1f74072728d2739601eb4f1b91415267
DiogoCC7 <dassuncao1234@gmail.com>
2025-11-11 22:43:33 +0000

    chore: update route, auth -> account

--- internal/endpoints.go
+++ internal/endpoints.go
@@ -13,7 +13,7 @@ import (
 )
 
 const (
-	AuthRoutes         = "auth"
+	AuthRoutes         = "account"
 	ProfileRoutes      = "profiles"
 	AvailabilityRoutes = "availability"
 	GroupRoutes        = "groups"
